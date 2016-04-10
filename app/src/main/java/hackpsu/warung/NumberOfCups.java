9ca2815a5b562febfce3b01bb1e56b0bc0a0a24c
Carlin <carlinau@gmail.com>
2016-04-10 14:53:22 -0400

    Fixed Number Picker GJ Toni

--- app/src/main/java/hackpsu/warung/NumberOfCups.java
+++ app/src/main/java/hackpsu/warung/NumberOfCups.java
@@ -5,15 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
 
 public class NumberOfCups extends AppCompatActivity {
 
     private static Button doneBtn;
+    NumberPicker numberpicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_of_cups);
         onClickButtonListener();
+        numberpicker = (NumberPicker) findViewById(R.id.numberPicker);
+        numberpicker.setMinValue(0);
+        numberpicker.setMaxValue(10);
+        numberpicker.setWrapSelectorWheel(false);
     }
 
     public void onClickButtonListener(){
